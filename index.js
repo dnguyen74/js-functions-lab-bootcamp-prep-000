a928b72479f20c9a24df3c2b6be22d4acb8f14ff
Dat Nguyen <dnguyen74@gmail.com>
2018-02-11 02:01:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function happyHolidayTo (holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountDown(holiday, day) {
-  return "It's ${days} days until ${holiday}!"
+function holidayCountDown(holiday, days) {
+  return 'It\'s ${days} days until ${holiday}!'
 }
